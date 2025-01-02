938c9df3fe611dde8cfd9a40100cf0be52142bcf
MattMilfont <matheusnmilfontc@gmail.com>
2025-01-02 16:15:38 -0300

    [Feature] Habilitando o CORS no back-end

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // Permite apenas este domínio
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    credentials: true, // Permite cookies e credenciais
+  });
+
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
